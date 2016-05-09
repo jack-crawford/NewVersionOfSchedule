0e1c1b37354c9afa4376c8dad2171b2ab1938b4f
Jack Laptop <jackcrawford2008@gmail.com>
2016-05-09 15:58:28 -0500

    Revert " lhgl'h;"
    This reverts commit ca8251914d01944964f6bdba0ee084301b5d2728.

--- Schedule App/schedule/schedule/ViewController.swift
+++ Schedule App/schedule/schedule/ViewController.swift
@@ -1,4 +1,4 @@
-    //
+//
 //  ViewController.swift
 //  schedule
 //
@@ -31,8 +31,6 @@ class ViewController: UIViewController {
             date_label.textColor = UIColor.blackColor();
             visual_separation.textColor = UIColor.blackColor();
             self.view.backgroundColor = uiwhite;
-            UIApplication.sharedApplication().statusBarStyle = .Default
-
         } else {
             date_label.textColor = uiwhite;
             visual_separation.textColor = schoolred;
@@ -41,8 +39,6 @@ class ViewController: UIViewController {
             mod_display.textColor = schoolblue;
             message_label.textColor = schoolblue;
             self.view.backgroundColor = UIColor.blackColor()
-            UIApplication.sharedApplication().statusBarStyle = .LightContent
-
         }
         print("button")
         counter = counter + 1;
